78b5dd560e7261c37d65652f1b72708769e50426
Stiven Villarreal <stivenvillarreal@gmail.com>
2026-02-17 01:24:32 -0500

    feat: agregar endpoints y esquemas para la gestión de órdenes en la API

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -2,14 +2,14 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.domain.services.product_service import ProductService
+from app.domain.services.order_service import OrderService
 
-# from app.domain.services.order_service import OrderService
 # from app.domain.services.report_service import ReportService
 from app.infrastructure.cache.redis_cache_adapter import RedisCacheAdapter
 from app.infrastructure.cache.redis_client import get_redis
 from app.infrastructure.database.connection import get_db
 
-# from app.infrastructure.database.repositories.order_repository import OrderRepository
+from app.infrastructure.database.repositories.order_repository import OrderRepository
 from app.infrastructure.database.repositories.product_repository import (
     ProductRepository,
 )
@@ -28,17 +28,17 @@ async def get_product_service(
     return ProductService(repo=repo, cache=cache)
 
 
-# async def get_order_service(
-#     db: AsyncSession = Depends(get_db),
-#     cache: RedisCacheAdapter = Depends(get_cache_adapter),
-# ) -> OrderService:
-#     product_repo = ProductRepository(db)
-#     order_repo = OrderRepository(db)
-#     return OrderService(
-#         order_repo=order_repo,
-#         product_repo=product_repo,
-#         cache=cache,
-#     )
+async def get_order_service(
+    db: AsyncSession = Depends(get_db),
+    cache: RedisCacheAdapter = Depends(get_cache_adapter),
+) -> OrderService:
+    product_repo = ProductRepository(db)
+    order_repo = OrderRepository(db)
+    return OrderService(
+        order_repo=order_repo,
+        product_repo=product_repo,
+        cache=cache,
+    )
 
 
 # async def get_report_service(
